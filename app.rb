c646871874e29d19fb32ad03ab0c3f1f7db777e5
Mark A Guza <guzamaguza@gmail.com>
2020-05-08 18:22:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require_relative 'config/environment'
 
+#this is our controller 
 class App < Sinatra::Base
 
+  #routes below 
 	get '/' do
 		  erb :index
 	end
+
+  get "/info" do
+  "Testing the info page"
+  end
+
 end
